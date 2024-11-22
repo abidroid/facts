0763bc2ae20a438105c7ab9c07702194cb8b5140
Muhammad Abid <abidroid1@gmail.com>
2024-11-22 22:09:52 +0500

    data loaded from facts.txt file to documents variable

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("yes")
+from langchain_community.document_loaders import TextLoader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+loader = TextLoader("facts.txt")
+docs = loader.load()
+print(docs)
